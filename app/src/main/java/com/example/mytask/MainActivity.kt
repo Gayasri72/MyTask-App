3dc41ab22dca406ec22d0d06113bbabbad744517
Gayasri72 <gayasripethum1@gmail.com>
2024-05-12 07:39:15 +0530

    my task xml updates

--- app/src/main/java/com/example/mytask/MainActivity.kt
+++ app/src/main/java/com/example/mytask/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.mytask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mytask.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: TaskDatabaseHelper
+    private lateinit var taskAdapter: TaskAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = TaskDatabaseHelper(this)
+        taskAdapter = TaskAdapter(db.getAllTasks(), this)
+
+        binding.taskRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.taskRecyclerView.adapter = taskAdapter
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this,AddTaskActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        taskAdapter.refreshData(db.getAllTasks())
     }
 }
---
+++ app/src/main/java/com/example/mytask/TaskItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mytask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TaskItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.task_item)
+    }
+}
